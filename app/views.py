345e5fb751125cc6bfe5d1ab30734b740a8203ea
Ian Robertson <ian.robertson@capitalone.com>
2016-11-16 08:47:06 -0500

    add preamble choice

--- app/views.py
+++ app/views.py
@@ -1,13 +1,32 @@
-from flask import render_template, flash, redirect
-from app import app
+"""Views from the app"""
+
 import json
 import random
+from flask import render_template
+from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    with open('app/static/peeps/peeps.json') as f:
-        peeps = json.load(f)
+    """Base page"""
+    with open('app/static/peeps/peeps.json') as peeps_file:
+        peeps = json.load(peeps_file)
         peep = random.choice(peeps)
-    return render_template('index.html',
-                           peep = peep)
+
+    preambles = (
+        "Did you guess it?",
+        "I bet you got it...",
+        "Did I stump you?",
+        "Obviously,",
+        "Oh right,"
+        "You're too good,"
+        "Hope you're having fun!"
+        "Say my name."
+        "Who am I?"
+        "Whaddaya know,"
+        )
+    answer_preamble = random.choice(preambles)
+
+    return render_template(
+        'index.html', peep=peep, answer_preamble=answer_preamble)
